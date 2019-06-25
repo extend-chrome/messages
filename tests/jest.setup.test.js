3e6103b685208dbdc59ef067333c7a0fcd191435
Jack Steam <teamsteamdev@gmail.com>
2019-06-25 10:19:38 -0500

    Tweak tests setup

--- tests/jest.setup.test.js
+++ tests/jest.setup.test.js
@@ -1,5 +1,14 @@
 test('chrome is mocked', () => {
   expect(chrome).toBeDefined()
+
+  expect(chrome.runtime).toBeDefined()
+  expect(chrome.contextMenus).toBeDefined()
+  expect(chrome.storage).toBeDefined()
+  expect(chrome.tabs).toBeDefined()
+  expect(chrome.webRequest).toBeDefined()
+  expect(chrome.proxy).toBeDefined()
+  expect(chrome.browserAction).toBeDefined()
+  expect(chrome.browserAction.enable()).toBeUndefined()
 })
 
 test('chromep to be promisified', () => {
