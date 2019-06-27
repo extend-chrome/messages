92141cc53ca79db7be9b59330fe5b41563076411
Jack Steam <teamsteamdev@gmail.com>
2019-06-27 09:00:17 -0500

    Wire up onlySend
    Co-authored-by: Amy Steam <amysteamux@gmail.com>

--- src/send.ts
+++ src/send.ts
@@ -2,21 +2,21 @@ import { uuidv4 as uuid } from './uuidv4'
 import { ports } from './ports'
 import { frameName } from './frames'
 
-/**
- * Send a message to another script. Use when you need to get a response.
- *
- * @returns A promise that resolves when the event listener calls `sendResponse`, or rejects when the event listener throws an error.
- * @param message Send any JSON-ifiable data.
- * @param target "background", "options", "popup", or a custom target name as a `string` or a tab id as a `number`.
- */
-export const send: SendMessage = (message, target) => {
-  const _target = target || frameName
+const setupSend = (
+  message: JsonifiableData,
+  target: TargetName | undefined,
+): { _message: JsonifiableMessage; port: Port } => {
+  const _target = target === undefined ? frameName : target
   const port = ports.get(_target)
 
-  if (!port) {
-    return Promise.reject(
-      new Error(`The port "${_target}" is not registered`),
+  if (!_target) {
+    throw new Error(
+      'Could not derive target port name or port name is empty string',
     )
+  }
+
+  if (!port) {
+    throw new Error(`The port "${_target}" is not registered`)
   }
 
   const _message: JsonifiableMessage = {
@@ -26,16 +26,31 @@ export const send: SendMessage = (message, target) => {
     only: false,
   }
 
-  return new Promise((resolve, reject) => {
+  return { _message, port }
+}
+
+/**
+ * Send a message to another script. Use when you need to get a response.
+ *
+ * @returns A promise that resolves when the event listener calls `sendResponse`, or rejects when the event listener throws an error.
+ * @param message Send any JSON-ifiable data.
+ * @param target "background", "options", "popup", or a custom target name as a `string` or a tab id as a `number`.
+ */
+export const send: SendMessage = (message, target) =>
+  new Promise((resolve, reject) => {
     try {
-      const handleMessage = ({
+      const { _message, port } = setupSend(message, target)
+
+      port.onMessage.addListener(handleMessage)
+      port.postMessage(_message)
+
+      function handleMessage({
         id,
         success,
         payload,
-      }: JsonifiableResponse) => {
+      }: JsonifiableResponse) {
         if (id === _message.id) {
           port.onMessage.removeListener(handleMessage)
-
           if (success === true) {
             resolve(payload)
           } else if (success === false) {
@@ -43,15 +58,10 @@ export const send: SendMessage = (message, target) => {
           }
         }
       }
-
-      port.onMessage.addListener(handleMessage)
-
-      port.postMessage(_message)
     } catch (error) {
       reject(error)
     }
   })
-}
 
 /**
  * Send a message to another script. Use when you don't need a response.
@@ -59,4 +69,15 @@ export const send: SendMessage = (message, target) => {
  * @param message Send any JSON-ifiable data.
  * @param target "background", "options", "popup", or a custom target name as a `string` or a tab id as a `number`.
  */
-export const onlySend: SendOnlyMessage = (message, target) => {}
+export const onlySend: SendOnlyMessage = (message, target) =>
+  new Promise((resolve, reject) => {
+    try {
+      const { _message, port } = setupSend(message, target)
+
+      port.postMessage(_message)
+
+      resolve()
+    } catch (error) {
+      reject(error)
+    }
+  })
--- tests/send.test.ts
+++ tests/send.test.ts
@@ -4,8 +4,12 @@ import { Port } from './jest.setup'
 
 jest.mock('../src/frames')
 
+beforeEach(() => {
+  ports.clear()
+})
+
 describe('send', () => {
-  test('gets port by name', async () => {
+  test('sends message to target', async () => {
     const name1 = 'background'
     const port1 = Port(name1)
     ports.set(name1, port1)
@@ -28,7 +32,7 @@ describe('send', () => {
     expect(port2.postMessage).toBeCalled()
   })
 
-  test('resolves on matching response', async () => {
+  test('resolves on matching response id', async () => {
     expect.assertions(1)
 
     const name1 = 'background'
@@ -116,14 +120,94 @@ describe('send', () => {
 
     const payload = 'hey there'
 
-    return send(payload, name1).catch((reason) => {
-      expect(reason).toBe('some error')
-    })
+    expect(send(payload, name1)).rejects.toBe('some error')
   })
 })
 
 describe('onlySend', () => {
-  test.todo('sends message')
-  test.todo('resolves if no errors')
-  test.todo('rejects if something throws')
+  test('sends message to target', async () => {
+    const name1 = 'background'
+    const port1 = Port(name1)
+    ports.set(name1, port1)
+
+    const name2 = 'options'
+    const port2 = Port(name2)
+    ports.set(name2, port2)
+
+    const message: JsonifiableMessage = {
+      id: 'asdf12334',
+      target: 'options',
+      payload: {
+        greeting: 'set-options',
+      },
+      only: true,
+    }
+    onlySend(message.payload, message.target)
+
+    expect(port1.postMessage).not.toBeCalled()
+    expect(port2.postMessage).toBeCalled()
+  })
+
+  test('resolves immediately', async () => {
+    expect.assertions(1)
+
+    const name1 = 'background'
+    const port1 = Port(name1)
+    ports.set(name1, port1)
+
+    const payload = 'hey there'
+    const result = await onlySend(payload, name1)
+
+    expect(result).toBeUndefined()
+  })
+
+  test('rejects if setupSend throws', async () => {
+    expect.assertions(1)
+
+    const name1 = 'background'
+    const port1 = Port(name1)
+    ports.set(name1, port1)
+
+    const payload = 'hey there'
+    const target = 'options'
+
+    expect(onlySend(payload, target)).rejects.toEqual(
+      new Error(`The port "${target}" is not registered`),
+    )
+  })
+
+  test('rejects if cannot derive target', async () => {
+    expect.assertions(1)
+
+    const name1 = 'background'
+    const port1 = Port(name1)
+    ports.set(name1, port1)
+
+    const payload = 'hey there'
+
+    // Simulate unable to derive port name
+    expect(onlySend(payload, '')).rejects.toEqual(
+      new Error(
+        'Could not derive target port name or port name is empty string',
+      ),
+    )
+  })
+
+  test('rejects if postMessage throws', async () => {
+    expect.assertions(1)
+
+    const name1 = 'background'
+    const port1 = Port(name1)
+    ports.set(name1, port1)
+
+    const postError = new Error('postError')
+    // @ts-ignore postMessage is jest.fn()
+    port1.postMessage.mockImplementation(() => {
+      throw postError
+    })
+
+    const payload = 'hey there'
+
+    expect(onlySend(payload, name1)).rejects.toBe(postError)
+  })
 })
