24b46163fb0adfeac42eee3d8efe94eb08105519
Jack Steam <teamsteamdev@gmail.com>
2019-07-16 08:58:18 -0500

    Update types and jest.setup

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,21 +1,11 @@
 import * as chrome from 'sinon-chrome'
 
-chrome.runtime.id = 'foo123' // Fix for schema.json
-
-// Some time around Chrome 71, extension.sendRequest (and a few others) started
-// throwing an error when accessed.  This mimics that behavior.
-Object.defineProperty(chrome.extension, 'sendRequest', {
-  get: function() {
-    throw new Error('Deprecated!')
-  },
-})
-
 var ChromePromise = require('chrome-promise/constructor')
-
 var chromep = new ChromePromise({ chrome })
 ;(<any>window).chrome = chrome
 ;(<any>window).chromep = chromep
 
+// Jest's jsdom does not include window.crypto
 var nodeCrypto = require('crypto')
 ;(<any>window).crypto = {
   getRandomValues: function(buffer: Uint8Array) {
