45c9bccb197180a0352f8e271d97ef4152ed7f39
Jack Steam <teamsteamdev@gmail.com>
2019-08-22 21:20:29 -0500

    Tweak configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,26 +12,8 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'comma-dangle': [
-      'warn',
-      {
-        arrays: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'always-multiline',
-        imports: 'always-multiline',
-        objects: 'always-multiline',
-      },
-    ],
     'no-console': 'off',
-    'no-unused-vars': [
-      'error',
-      {
-        args: 'after-used',
-        ignoreRestSiblings: true,
-        vars: 'all',
-      },
-    ],
-    quotes: ['warn', 'single'],
-    semi: ['warn', 'never'],
+    quotes: 'off',
+    semi: 'off',
   },
 }
