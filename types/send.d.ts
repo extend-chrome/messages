9259c810136b879b1aec7cdfbc332d9325404f1d
Jack Steam <teamsteamdev@gmail.com>
2019-08-25 16:54:11 -0500

    gitignore types/

--- types/send.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const send: (
-  message: any,
-  target?: string | number | undefined,
-) => Promise<void>
-export declare const asyncSend: (
-  message: any,
-  target?: string | number | undefined,
-) => Promise<any>
