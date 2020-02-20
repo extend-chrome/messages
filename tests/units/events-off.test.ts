cf75a46d3444d9d7330bb8fd25bffd47b6757e41
Jack Steam <jacksteamdev@gmail.com>
2020-02-20 08:14:51 -0600

    Update events-off tests

--- tests/units/events-off.test.ts
+++ tests/units/events-off.test.ts
@@ -1,45 +1,25 @@
+import { chrome } from '@bumble/jest-chrome'
+import { _getListener, _listeners } from '../../src/ListenerMap'
 import { useScope } from '../../src/scope'
-
-import { _listeners, _getListener } from '../../src/ListenerMap'
-
-import * as chrome from 'sinon-chrome'
-import assert from 'power-assert'
-import {
-  AsyncMessageListener,
-  MessageListener,
-} from '../../src/types'
+import { AsyncMessageListener, MessageListener } from '../../src/types'
 
 const scope = 'test'
 const messages = useScope(scope)
 
-let lastError: { message: string } | undefined
-const lastErrorSpy = jest.fn(() => lastError)
-Object.defineProperty(chrome.runtime, 'lastError', {
-  get: lastErrorSpy,
-})
-
-console.error = jest.fn()
-
 afterEach(() => {
-  lastError = undefined
-  chrome.reset()
+  jest.clearAllMocks()
   _listeners.clear()
 })
 
 test('removes messages.on listener', () => {
   const listener = jest.fn() as MessageListener
-  const asyncListener = jest.fn(
-    (x, y, z) => {},
-  ) as AsyncMessageListener
+  const asyncListener = jest.fn((x, y, z) => {}) as AsyncMessageListener
 
   messages.on(listener)
   messages.on(asyncListener)
   messages.off(listener)
 
-  assert(
-    chrome.runtime.onMessage.removeListener.called,
-    'chrome.runtime.onMessage.removeListener was not called',
-  )
+  expect(chrome.runtime.onMessage.hasListener(listener)).toBe(false)
 
   const _listener = _getListener(scope, listener)
   const _asyncListener = _getListener(scope, asyncListener)
@@ -50,18 +30,13 @@ test('removes messages.on listener', () => {
 
 test('removes messages.asyncOn listener', () => {
   const listener = jest.fn() as MessageListener
-  const asyncListener = jest.fn(
-    (x, y, z) => {},
-  ) as AsyncMessageListener
+  const asyncListener = jest.fn((x, y, z) => {}) as AsyncMessageListener
 
   messages.on(listener)
   messages.on(asyncListener)
   messages.off(asyncListener)
 
-  assert(
-    chrome.runtime.onMessage.removeListener.called,
-    'chrome.runtime.onMessage.removeListener was not called',
-  )
+  expect(chrome.runtime.onMessage.hasListener(asyncListener)).toBe(false)
 
   const _listener = _getListener(scope, listener)
   const _asyncListener = _getListener(scope, asyncListener)
