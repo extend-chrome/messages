a8d04980fbddd5eb5597701f1549f4214ca27246
Jack Steam <jacksteamdev@gmail.com>
2021-05-25 15:44:37 -0500

    chore: upgrade rxjs and switch to peerDep tests: upgrade typescript and ts-jest

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      packageJson: 'package.json',
-      tsConfig: 'tsconfig.json',
-    },
-  },
   setupFilesAfterEnv: ['./tests/jest.setup.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/playground/'],
 }
